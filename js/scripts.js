1bfbee446fe78a3eac0327dafdee1a64d446f02a
mhyneman8 <mhyneman@gmail.com>
2021-03-14 22:32:43 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// add modal for bingo cards
+
+
+// business API
+cont businessList = (function() {
+  let businessList = [];
+  let apiUrl = 'https://maps.googleapis.com/maps/api/place/details/output?parameters'
+})
